cd21819958b9df07d8e78ba2a7e3f9e8b918a63d
dach816 <sdachinger@gmail.com>
2016-08-02 18:41:52 -0400

    Created a new activity for inception tic tac toe and created the layout

---
+++ app/src/main/java/com/hfad/inceptionttt/InceptionGameActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.inceptionttt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InceptionGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inception_game);
+    }
+}
